19744040992586d2a89fee646b8a249839088acf
Pereira, Meghna <Meghna.Pereira+cdk@cdk.com>
2025-12-04 13:19:01 +0530

    Add /health endpoint

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send("Hello from Express.js!");
 });
 
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', time: new Date().toISOString() });
+});
+
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
